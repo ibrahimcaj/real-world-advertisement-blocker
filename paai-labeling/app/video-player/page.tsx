07778fee79639b860829b956d94dd4829f850a29
Ibrahim <ibrahimcajlakovic1@gmail.com>
2026-05-21 09:00:00 +0200

    add app/video-player/page.tsx

---
+++ paai-labeling/app/video-player/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { VideoDetector } from "@/components/video-detector";
+
+// put your yolo class names here in order
+const CLASS_NAMES: string[] = [
+  // "person", "car", "dog", ...
+];
+
+export default function VideoPlayerPage() {
+  return (
+    <div className="h-screen flex flex-col">
+      <VideoDetector classNames={CLASS_NAMES.length ? CLASS_NAMES : undefined} />
+    </div>
+  );
+}
