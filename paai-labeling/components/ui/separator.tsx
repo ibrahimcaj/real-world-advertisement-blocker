9f48807e5d440f5e92f3baef9015c6f9d8ee375d
Ibrahim <ibrahimcajlakovic1@gmail.com>
2026-05-25 09:00:00 +0200

    add components/ui/separator.tsx skeleton

---
+++ paai-labeling/components/ui/separator.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { Separator as SeparatorPrimitive } from "@base-ui/react/separator";
+import { cn } from "@/lib/utils";
+
+function Separator({ className, ...props }: SeparatorPrimitive.Props) {
+  return <SeparatorPrimitive className={cn("shrink-0 bg-border", className)} {...props} />;
+}
+
+export { Separator };
